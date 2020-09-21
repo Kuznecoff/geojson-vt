c50d562b0f10f598e2058f70b9552b3d5503d459
Andrey Kuznecov <a.kuznecov@2Gis.ru>
2020-09-22 03:59:44 +0700

    add comment

--- types.d.ts
+++ types.d.ts
@@ -60,5 +60,19 @@ export interface GeoJsonVT {
     getTile(z: number, x: number, y: number): TileGeoJsonVt | null;
 }
 
+/**
+ * Data: GeoJSON
+ * Options: 
+ * 	maxZoom: 14,  max zoom to preserve detail on; can't be higher than 24
+ *	tolerance: 3, simplification tolerance (higher means simpler)
+ *	extent: 4096, tile extent (both width and height)
+ *	buffer: 64,   tile buffer on each side
+ *	debug: 0,     logging level (0 to disable, 1 or 2)
+ *	lineMetrics: false, whether to enable line metrics tracking for LineString/MultiLineString features
+ *	promoteId: null,    name of a feature property to promote to feature.id. Cannot be used with `generateId`
+ *	generateId: false,  whether to generate feature ids. Cannot be used with `promoteId`
+ *	indexMaxZoom: 5,    max zoom in the initial tile index
+ *	indexMaxPoints: 100000 max number of points per tile in the index
+ */
 export default function geojsonvt(data: GeoJSON, options: GeoJsonVtOptions): GeoJsonVT
 
