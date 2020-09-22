5f6d138c4a3cc0a4f142e09097d2811280172c7b
Andrey Kuznecov <a.kuznecov@2Gis.ru>
2020-09-22 11:03:18 +0700

    descriptions

--- types.d.ts
+++ types.d.ts
@@ -28,15 +28,54 @@ interface TileFeaturePoints {
 export type TileFeature = TileFeaturePoints | TileFeatureLines | TileFeaturePolygons;
 
 export interface GeoJsonVtOptions {
+  /**
+  * max zoom to preserve detail on; can't be higher than 24
+  */
     maxZoom?: number,
+
+  /**
+  * max zoom in the initial tile index
+  */
     indexMaxZoom?: number,
+
+  /**
+  * 100000 max number of points per tile in the index
+  */
     indexMaxPoints?: number,
+
+  /**
+  * tolerance: 3, simplification tolerance (higher means simpler)
+  */
     tolerance?: number,
+
+  /**
+  * 4096, tile extent (both width and height)
+  */
     extent?: number,
+
+    /**
+  * 64,   tile buffer on each side
+  */
     buffer?: number,
+
+  /**
+  * false, whether to enable line metrics tracking for LineString/MultiLineString features
+  */
     lineMetrics?: boolean,
+
+  /**
+  * null,    name of a feature property to promote to feature.id. Cannot be used with `generateId`
+  */
     promoteId?: number | null,
+
+  /**
+  * false,  whether to generate feature ids. Cannot be used with `promoteId`
+  */
     generateId?: boolean,
+
+  /**
+  * 0,     logging level (0 to disable, 1 or 2)
+  */
     debug?: number
 }
 
@@ -60,19 +99,5 @@ export interface GeoJsonVT {
     getTile(z: number, x: number, y: number): TileGeoJsonVt | null;
 }
 
-/**
- * Data: GeoJSON
- * Options: 
- * 	maxZoom: 14,  max zoom to preserve detail on; can't be higher than 24
- *	tolerance: 3, simplification tolerance (higher means simpler)
- *	extent: 4096, tile extent (both width and height)
- *	buffer: 64,   tile buffer on each side
- *	debug: 0,     logging level (0 to disable, 1 or 2)
- *	lineMetrics: false, whether to enable line metrics tracking for LineString/MultiLineString features
- *	promoteId: null,    name of a feature property to promote to feature.id. Cannot be used with `generateId`
- *	generateId: false,  whether to generate feature ids. Cannot be used with `promoteId`
- *	indexMaxZoom: 5,    max zoom in the initial tile index
- *	indexMaxPoints: 100000 max number of points per tile in the index
- */
 export default function geojsonvt(data: GeoJSON, options: GeoJsonVtOptions): GeoJsonVT
 
